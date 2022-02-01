df59a561c5fcce5890bbf8a241e8e84fa41df92a
mauag <mauricio.altamirano@banregio.com>
2022-02-01 12:16:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tic-tac-toe-quknbn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tic-tac-toe-quknbn)
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { GameService } from '../game.service';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <div 
+      class="game-square rounded-lg border bg-teal-lightest 
+      shadow-md " 
+      (click)="changePlayer()" 
+      [ngClass]="{noClick: gameService.winner} "
+    >
+      <p class="text-grey-darker"> {{ square.state}} </p>
+    </div>
+   `,
+  styles: [
+    `
+    .game-square { 
+      height: 96%; 
+      text-align: center;
+      line-height: 0.85;
+      cursor: pointer;
+    }
+
+    p { 
+      display: inline;
+      margin: 0px;
+      font-size: 14rem;
+      overflow: hidden;
+    }
+
+    .noClick {
+      pointer-events: none;
+    }`,
+  ],
+})
+export class SquareComponent implements OnInit {
+  @Input() square;
+
+  constructor(public gameService: GameService) {}
+
+  ngOnInit() {
+    console.log("cuadro",this.square);
+  }
+
+  changePlayer() {
+    this.gameService.isGameRunning = true;
+
+    if (this.gameService.isGameRunning && this.square.state === null) {
+      this.square.state = this.gameService.activePlayer;
+      this.gameService.changePlayerTurn(this.square);
+    }
+  }
+}
